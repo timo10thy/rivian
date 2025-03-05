1681bd434fbaacbd99b85049e7fcd2ad0aefe5ee
Timothy Ogory <timothyogory@gmail.com>
2025-03-05 23:21:19 +0100

    first tailwind code

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "Roboto":['Montserrat', 'sans-serif'] 
+      }
+    },
+  },
+  plugins: [],
+}
